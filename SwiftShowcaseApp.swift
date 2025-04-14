96c2c2f1cef5ed3da5f9a8187c6d0d13bf64e47a
amogh-joshi-wac <amogh.joshi@waclighting.com>
2025-04-14 16:20:06 -0500

    Initial commit

---
+++ SwiftShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShowcaseApp.swift
+//  SwiftShowcase
+//
+//  Created by Amogh Josh on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
