23a48a723b497b74d888b6d07df55a189781eade
amogh-joshi-wac <amoghjoshi1999@gmail.com>
2025-04-15 23:52:14 -0500

    moved things around

---
+++ SwiftShowcase/Views/SpeciesView/SpeciesListView/SpeciesListView.swift
@@ -0,0 +1,18 @@
+//
+//  SpeciesListView.swift
+//  SwiftShowcase
+//
+//  Created by Amogh Josh on 4/15/25.
+//
+
+import SwiftUI
+
+struct SpeciesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeciesListView()
+}
